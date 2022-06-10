59d30456fd87da6e33ae67895d62544f20f9f246
HoJun-Seo <ghwns6659@gmail.com>
2022-06-10 23:53:01 +0900

    Calling_Diary-3 회원 관련 기본 기능 구현 - 회원가입 폼 작업 중

--- src/main/resources/static/js/memberController.js
+++ src/main/resources/static/js/memberController.js
@@ -1,18 +1,11 @@
+// 약관 동의서 함수
 function chk() {
-    var form = document.form;
-    var req1 = form.req1.checked;
-    var req2 = form.req2.checked;
-    var flag = 0;
-    if(req1 == true && req2 == true) {
-        flag = 1;
-    }
-    if(flag == 1) {
-        window.close();
-        form.method = "get";
-        form.action = "/move/registerForm";
-        form.target = "self";
-        form.submit();
+    let req1 = $("#req1").is(":checked");
+    let req2 = $("#req2").is(":checked");
 
+    if(req1 == true && req2 == true) {
+        window.close();
+        opener.location.href = "/move/registerForm";
     }else {
         alert("필수 약관에 동의하셔야 합니다.");
     }
@@ -22,3 +15,10 @@ function nochk(){
     alert("동의하지 않으면 가입하실 수 없습니다.");
     window.close();
 }
+$(function(){
+    $("#myModal").modal({
+        show : true,
+        backdrop : false,
+        keyboard : false
+    });
+})
