aabb5dd3751309b63f5bc952de01d2754381d4a7
HoJun-Seo <ghwns6659@gmail.com>
2022-07-12 06:16:27 +0900

    Calling_Diary-17 일정 즐겨찾기 기능 구현 - 마이페이지 에서 작성한 일정 전체조회 및 출력기능 구현

--- src/main/java/Personal_Project/Calling_Diary/controller/EventController.java
+++ src/main/java/Personal_Project/Calling_Diary/controller/EventController.java
@@ -112,4 +112,36 @@ public class EventController {
         }
         return "deleteSuccess";
     }
+
+    @PostMapping("/pagination")
+    public String pagination(@RequestBody String httpbody, Model model){
+
+        JSONObject jsonObject = new JSONObject(httpbody);
+        String event = jsonObject.getString("event");
+        int index = jsonObject.getInt("index");
+
+        JSONArray jsonArray = new JSONArray(event);
+        JSONArray pageArray = new JSONArray();
+
+        // 배열의 길이에 따른 페이징은 이미 되어있으므로 인덱스 값을 활용하여 배열을 새로 만들어준 뒤 html 파일에 넘겨주는 것만 구현한다.
+        //첫 페이지, 또는 중간 페이지인 경우
+        if(index*5 < jsonArray.length()){
+            for (int i = jsonArray.length()-((index*5)-4); i >= jsonArray.length()-(index*5); i--){
+                pageArray.put(jsonArray.get(i));
+            }
+        }
+        // 마지막 페이지인 경우
+        else{
+            System.out.println("마지막 페이지");
+            for (int i = jsonArray.length()-((index*5)-4); i >= 0; i--){
+                pageArray.put(jsonArray.get(i));
+            }
+        }
+
+        String pagination = pageArray.toString();
+        pagination = pagination.replace("\"", "\\\"");
+        model.addAttribute("pageArray", pagination);
+
+        return "calendar/eventPagination";
+    }
 }
--- src/main/resources/static/js/calendarScript.js
+++ src/main/resources/static/js/calendarScript.js
@@ -152,3 +152,65 @@ function callEventMypage() {
             }
         })
 }
+
+function callEventMypageFull(){
+
+    fetch("/events/" + member.uid)
+        .then((response) => response.text())
+        .then((data) => {
+
+            if(data === "eventNotExist"){
+                $(".eventPrintFullTab").html("<h4 class=\"title\">작성한 일정이 없습니다.</h4>");
+            }
+            else{
+
+                let event = JSON.parse(data);
+
+                // 배열의 길이에 따라서 페이지의 갯수가 달라져야 한다.
+                // 각 번호를 클릭 했을 때 기존의 링크는 비활성화 되고 클릭한 링크는 활성화 되어야 한다.
+                // 작성되어 있는 일정은 모두 가져온 상태이므로, 배열의 길이에 따라 각 페이지에서 다르게 출력 시켜줘야 한다.
+                let pageCount = event.length/5;
+                if(pageCount <= 1)
+                    pageCount = 1;
+                else
+                    pageCount = Math.ceil(pageCount);
+
+                $(".eventPrintFull").append("<li><a href=\"#\" onclick=\"eventList("+(1).toString()+")\" aria-label=\"Previous\"><span aria-hidden=\"true\">&laquo;</span></a></li>");
+                for(let index = 1; index <= pageCount; index++){
+                    // a 태그의 링크를 통해 각 페이지에 맞는 일정들을 출력해줘야 한다.
+                    $(".eventPrintFull").append("<li class=\""+index.toString()+"\"><a href=\"javascript:eventList("+index.toString()+");\">" + index.toString() + "</a></li>")
+                }
+
+                eventList("1");
+                $(".eventPrintFull").append("<li><a href=\"#\" onclick=\"eventList("+pageCount.toString()+")\" aria-label=\"Next\"><span aria-hidden=\"true\">&raquo;</span></a></li>");
+            }
+        })
+}
+
+function eventList(index){
+
+    fetch("/events/" + member.uid)
+        .then((response) => response.text())
+        .then((data) => {
+
+            if(data === "eventNotExist"){
+                $(".eventPrintFullTab").html("<h4 class=\"title\">작성한 일정이 없습니다.</h4>");
+            }
+            else{
+                fetch("/events/pagination/",{
+                    method:"post",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        "event":data,
+                        "index":Number(index)
+                    })
+                })
+                .then((response) => response.text())
+                .then((data) => {
+                    $(".eventPrintFullTab").html(data);
+                });
+            }
+        })
+}
